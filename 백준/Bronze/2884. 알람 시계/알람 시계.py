1233e227a23c3ecdf644aa1ba80deb564662496b
Lim JaeKwon <79525438+JaeKwonLim@users.noreply.github.com>
2022-10-24 15:30:25 +0900

    [Bronze III] Title: 알람 시계, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,13 @@
+H, M = map(int, input().split())
+
+if M < 45:
+    if H == 0:
+        print(23, 60 + (M - 45))
+    else:
+        print(H - 1,60 + (M - 45))
+
+elif M == 45:
+    print(H, 0)
+
+else:
+    print(H, M - 45)
